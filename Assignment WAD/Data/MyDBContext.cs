1dd83b3f45c5b89e085733795b5d81a8b5754a2f
ducbe <ducbe2k2@gmail.com>
2021-12-11 12:38:36 +0700

    first commit

---
+++ Assignment WAD/Data/MyDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Assignment_WAD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StudentManage.Data
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext():base("name=T2009MTest")
+        {
+
+        }
+        public DbSet<StudentLate> Students { get; set; }
+    }
+}
---
+++ Assignment WAD/Models/StudentLate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_WAD.Models
+{
+    public class StudentLate
+    {
+        [DisplayName("#")]
+        public int Id { get; set; }
+        [DisplayName("Mã Sinh Viên")]
+        [Required(ErrorMessage = "Vui Lòng Nhập Mã Sinh Viên")]
+        public string rollNumber { get; set; }
+        [DisplayName("Họ Và Tên")]
+        [Required(ErrorMessage = "Vui Lòng Nhập Họ Tên")]
+        public string fullName { get; set; }
+        [Required(ErrorMessage = "Vui Lòng Chọn Phương Thức Đóng Phạt")]
+        [DisplayName("Phương Thức Đóng Phạt")]
+        public string Method { get; set; }
+        [Required(ErrorMessage = "Vui Lòng Nhập Phương Thức Đóng Phạt")]
+        [DisplayName("Nộp Phạt")]
+        public string Fines { get; set; }
+        [Required(ErrorMessage = "Vui Lòng Nhập Nguyên Nhân Đóng Phạt")]
+        [DisplayName("Nguyên Nhân")]
+        public string Cause { get; set; }
+        [DisplayName("Ngày Nộp Phạt")]
+        public DateTime Date { get; set; }
+    }
+}
