e74fdc76a141ccd667fbadd77f377492fc166ee9
ducbe <ducbe2k2@gmail.com>
2021-12-11 12:58:23 +0700

    first commit

---
+++ Assignment WAD/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Assignment_WAD.Models;
+using StudentManage.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_WAD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyDBContext myDBContext = new MyDBContext();
+        public ActionResult Index()
+        {
+            var result = myDBContext.Students.ToList();
+            return View(result);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "rollNumber,fullName,Fines,Cause")] StudentLate st) {
+            if (ModelState.IsValid)
+            {
+                myDBContext.Students.Add(st);
+                myDBContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+    }
+}
